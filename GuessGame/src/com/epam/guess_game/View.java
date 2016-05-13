4c765ca33fdfdd2252f331452f39d25ee54309ad
MariaLapovska <maria.lapovsky@gmail.com>
2016-05-13 03:50:48 +0300

    Update View.java

--- GuessGame/src/com/epam/guess_game/View.java
+++ GuessGame/src/com/epam/guess_game/View.java
@@ -7,9 +7,9 @@ import java.util.ArrayList;
  */
 public class View {
     public static final String WRONG_BOUNDS_DATA = "Wrong boundaries %d and %d! " +
-            "Lower border should be less by three than upper!%n";
+            "Lower border should be less by three than upper!";
     public static final String GREETING_DATA = "Dear user, welcome to the more-less game!";
-    public static final String INPUT_INT_DATA = "Input INT value from %d to %d:%n";
+    public static final String INPUT_INT_DATA = "Input INT value from %d to %d:";
     public static final String WRONG_INPUT_INT_DATA = "Wrong input format!";
     public static final String OUT_OF_RANGE_DATA = "Input is out of range!";
     public static final String LESS_DATA = "This number is less then mine.";
@@ -24,7 +24,7 @@ public class View {
     }
 
     public void printMessageAndRange(String message, int min, int max) {
-        System.out.printf(message, min, max);
+        printMessage(String.format(message, min, max));
     }
 
     public void printStats(ArrayList<String> stats) {
